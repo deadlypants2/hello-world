caff64e47f042b2afada927f9c4888e9769d3906
deadlypants <remcovandijck@gmail.com>
2024-10-18 18:00:24 +0200

    04-10-2024

--- opdracht 6.2.py
+++ opdracht 6.2.py
@@ -6,8 +6,7 @@ vraag = str(input("Gaat u vandaag naar de dierentuin?"))
 if vraag=="ja":
     ask = int(input("Hoeveel mensen gaan met u meen naar de dierntuin? "))
 #dit is de loop waar de state word verhoogd met 1 waarna de status word uitgeprint
-    while state < ask:
-        state +=1
+    for i in range(ask):
         print("Veel plezier!")
 else:
     print("Een fijne dag verderr gewenst.")
