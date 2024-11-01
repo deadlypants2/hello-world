2069afdb6e24ab77b4005656c77c35d425ea4cbe
deadlypants <remcovandijck@gmail.com>
2024-11-01 11:07:37 +0100

    toets van 04-10-2024. Mogelijk oude bestanden kapot gemaakt maar boeit me niks meer

---
+++ Toets 1-11-2024.py
@@ -0,0 +1,49 @@
+#lengte = input("Hoe lang is je zwembad? (in meters")
+#breedte = input("Hoe breed is je zwembad? (in meters")
+#diepte = input("Hoe diep is je zwembad? (in meters")
+#inhoud = lengte * breedte * diepte
+#print("Jouw zwembad heeft als hij helemaal vol is", inhoud, "m3 water")
+
+#de juiste code is:
+
+#lengte = float(input("Hoe lang is je zwembad? (in meters"))
+#breedte = float(input("Hoe breed is je zwembad? (in meters"))
+#diepte = float(input("Hoe diep is je zwembad? (in meters"))
+#inhoud = lengte * breedte * diepte
+#print("Jouw zwembad heeft als hij helemaal vol is", inhoud, "m3 water")
+
+
+#foute code:
+
+#naam = input("Wat is je voornaam? ")
+#lengte = int(input("Hoe lang ben je ? (in meters)"))
+#print("Hallo", naam, "je bent", lengte, "meters")
+
+#de int moet een float zijn. Goede code:
+
+#naam = input("Wat is je voornaam? ")
+#lengte = float(input("Hoe lang ben je ? (in meters)"))
+#print("Hallo", naam, "je bent", lengte, "meters")
+
+#hierbij kanabaliseer ik de vorig opdracht:
+
+#vraag de aantal GPU's op aan de gebruiker en zet de int variablele op nul zodat deze gedifineerd is.
+aantalint = int(0)
+aantal = str(input("Hoeveel videokaarten zijn besteld?\n"))
+
+#controleer of er een goed getal is ingevuld.
+
+if aantal.isdigit():
+    aantalint = int(aantal)
+
+#print de levertijd en geef aan waneer er geen GPU besteld is.
+if aantalint > 0:
+    if aantalint < 10: print("De levertijd van uw bestelling bevat 3 dagen.")
+    elif aantalint >= 10 and aantalint < 20: print("De levertijd van uw bestelling bevat 1 week.")
+    elif aantalint >= 21 and aantalint < 50: print("De levertijd van uw bestelling bevat 2 weken.")
+    elif aantalint >= 50: print("Neem a.u.b. contact op met Nvidia voor uw bestelling.")
+    else: print("Voer het juiste aantal in a.u.b.")
+else:
+    print("Voer het juiste aantal in a.u.b.")
+
+
--- opdracht 6.2.py
+++ opdracht 6.2.py
@@ -2,7 +2,7 @@
 state = 0
 #Hierbij word de vraag gesteld of de persoon naar de dierntuin gaat.
 vraag = str(input("Gaat u vandaag naar de dierentuin?"))
-#hierbij word er gecontroleerd of er ja is beantwoord en word er gevraagthoeveel mensen naar de dierentuin gaan.
+#hierbij word er gecontroleerd of er ja is beantwoord en word er gevraagt hoeveel mensen naar de dierentuin gaan.
 if vraag=="ja":
     ask = int(input("Hoeveel mensen gaan met u meen naar de dierntuin? "))
 #dit is de loop waar de state word verhoogd met 1 waarna de status word uitgeprint
